e1806617b23e562a20faadcc8cb61c086da7fc00
Anish Aravind <anish.arvind407@gmail.com>
2020-03-17 22:31:42 +0530

    login and logout using cookie auth and google ..

--- ConfArch.Web/Controllers/AccountController.cs
+++ ConfArch.Web/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using ConfArch.Data.Repositories;
 using ConfArch.Web.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -32,6 +33,7 @@ namespace ConfArch.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginModel model)
         {
+            // we get the user
             var user = this.UserRepository.GetByUsernameAndPassword(model.Username, model.Password);
 
             if (user == null) return Unauthorized();
@@ -44,6 +46,13 @@ namespace ConfArch.Web.Controllers
                 new Claim("FavoriteColor", user.FavoriteColor)
             };
 
+           // httpcontext has claims principal, when we have different authentication mechanisms we will have more than
+           // one claimsIdentity, in this case we are filling in claims identity for cookie based auth
+           //  we create a claims principal using the claimsIdentity and sign in using our
+           // httpcontext.So our httpcontext will have claims principal with claims identity array
+           //with all the different claims. We have set default auth scheme to cookie,
+           //so this cookie will have different options like shud it be persistent or not
+           //  we can customize based on our needs.
             var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
             var principal = new ClaimsPrincipal(claimsIdentity);
 
@@ -53,6 +62,76 @@ namespace ConfArch.Web.Controllers
                  new AuthenticationProperties { IsPersistent = model.RememberLogin });
 
             return LocalRedirect(model.ReturnUrl);
+        }
+
+        [AllowAnonymous]
+        public IActionResult LoginWithGoogle(string returnUrl = "/")
+        {
+            var props = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("GoogleLoginCallback"),
+                Items =
+                {
+                    { "returnUrl", returnUrl }
+                }
+            };
+            return Challenge(props, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> GoogleLoginCallback()
+        {
+            // read google identity from the temporary cookie
+            // this cookie will have google claims
+            var result = await HttpContext.AuthenticateAsync(
+                ExternalAuthenticationDefaults.AuthenticationScheme);
+
+            // we read the claims and then 
+            var externalClaims = result.Principal.Claims.ToList();
+
+            //we get the google unique identifier for the user and then if the id doesnt 
+            // exist we can save it(for the first time). 
+           
+            var subjectIdClaim = externalClaims.FirstOrDefault(
+                x => x.Type == ClaimTypes.NameIdentifier);
+            var subjectValue = subjectIdClaim.Value;
+
+            //If the id exists in the DB for the user, then we get the user roles or 
+            // any other detail that we need from the db 
+            var user = UserRepository.GetByGoogleId(subjectValue);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("FavoriteColor", user.FavoriteColor)
+            };
+
+            // we create another claims principal from the claims and assign it to the 
+            // cookie from the old cookie scheme
+            var identity = new ClaimsIdentity(claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            // delete temporary cookie used during google authentication
+            await HttpContext.SignOutAsync(
+                ExternalAuthenticationDefaults.AuthenticationScheme);
+
+            // sign in using the cookie scheme, since we have implemented logout using the cookie scheme
+            // we will do this for all providers since we can follow the same scheme 
+            // and logout using the same scheme so that the cookie is removed safely
+            // else we will have to remove each cookie that we create for each login provider.
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            return LocalRedirect(result.Properties.Items["returnUrl"]);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/");
         }
     }
 }
--- ConfArch.Web/Startup.cs
+++ ConfArch.Web/Startup.cs
@@ -1,6 +1,7 @@
 using ConfArch.Data;
 using ConfArch.Data.Repositories;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Authorization;
@@ -23,23 +24,44 @@ namespace ConfArch.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews(options => options.Filters.Add(new AuthorizeFilter()));
-           
+
             services.AddScoped<IConferenceRepository, ConferenceRepository>();
             services.AddScoped<IProposalRepository, ProposalRepository>();
             services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
 
             services.AddDbContext<ConfArchDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
                     assembly => assembly.MigrationsAssembly(typeof(ConfArchDbContext).Assembly.FullName)));
 
             // we can add the different types of authentication 
             // for ex: cookie
             //we can give it a scheme name, by default the word cookie is used.
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(o=>
-                {
-                    o.LoginPath = "account/signIn";
-                });
+            services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme; // for reconstructing claims principaln
+                //commented this code as when we use multiple login providers then this will always 
+                // login from google
+                //o.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;// for login use google scheme
+                //o.DefaultForbidScheme = CookieAuthenticationDefaults.AuthenticationScheme;// use forbidden url from this scheme
+            })
+             .AddCookie(o =>
+             {
+                 o.LoginPath = "/Account/Login";
+             }).AddCookie(ExternalAuthenticationDefaults.AuthenticationScheme)
+             .AddGoogle(options =>
+             {
+                 // we add a new cookie scheme and tell google to use this scheme when login
+                 // we want google claims to be in a separate cookie 
+                 // check login from google to get more details
+                 // when we sign in using google this cookie will have all the details.
+                 // we read this cookie and create a new claims principal that will contain all 
+                 //the common claims needed by all login methods, since google will contain only its claims
+
+                 options.SignInScheme = ExternalAuthenticationDefaults.AuthenticationScheme;
+                 options.ClientId = Configuration["Google:ClientId"];
+                 options.ClientSecret = Configuration["Google:Secret"];
+             });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
